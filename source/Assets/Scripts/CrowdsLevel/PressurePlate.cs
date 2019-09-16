263d25992017e523f93e9017426ae2b6be6dce89
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2019-09-16 19:26:58 -0300

    Pressure Plate Agent Detection

--- source/Assets/Scripts/CrowdsLevel/PressurePlate.cs
+++ source/Assets/Scripts/CrowdsLevel/PressurePlate.cs
@@ -4,9 +4,60 @@ using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
+    public bool isEmpty = true;
+
+    [SerializeField] private Material _greenMaterial;
+    [SerializeField] private Material _redMaterial;
+    [SerializeField] private GameObject _line;
+    [SerializeField] private GameObject _buttonFrame;
+
+    private MeshRenderer _meshRenderer;
+    private int _contacts = 0;
+
+    private void Start()
+    {
+        _meshRenderer = GetComponentInChildren<MeshRenderer>();
+    }
+
     public Vector3 GetRandomPositionInside(float range)
     {
         Vector3 randomPosition = Random.insideUnitSphere * range;
         return new Vector3(this.transform.position.x + randomPosition.x, this.transform.position.y, this.transform.position.z + randomPosition.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _contacts++;
+        UpdateMaterial(isEmpty = _contacts == 0);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _contacts--;
+        UpdateMaterial(isEmpty = _contacts == 0);
+    }
+
+    private void UpdateMaterial(bool isEmpty)
+    {
+        if(isEmpty)
+        {
+            _meshRenderer.material = _greenMaterial;
+            _buttonFrame.GetComponentInChildren<MeshRenderer>().material = _greenMaterial;
+
+            for (int i = 0; i < _line.transform.childCount; i++)
+            {
+                _line.transform.GetChild(i).GetComponentInChildren<MeshRenderer>().material = _greenMaterial;
+            }
+        }
+        else
+        {
+            _meshRenderer.material = _redMaterial;
+            _buttonFrame.GetComponentInChildren<MeshRenderer>().material = _redMaterial;
+
+            for (int i = 0; i < _line.transform.childCount; i++)
+            {
+                _line.transform.GetChild(i).GetComponentInChildren<MeshRenderer>().material = _redMaterial;
+            }
+        }
     }
 }
