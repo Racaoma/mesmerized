1b686ec7f8b906706eb529563349acba06f10912
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2019-09-27 20:09:08 -0300

    Place Items in Pedestals

--- source/Assets/Scripts/CrowdsLevel/Pedestal.cs
+++ source/Assets/Scripts/CrowdsLevel/Pedestal.cs
@@ -4,8 +4,44 @@ using UnityEngine;
 
 public class Pedestal : InteractableItemBehaviour
 {
+    [SerializeField] private GameObject robotGameObject;
+    [SerializeField] private GameObject machineGameObject;
+    [SerializeField] private GameObject dogGameObject;
+    [SerializeField] private GameObject lampGameObject;
+
+    private GameObject _currentPlaceGameObject;
+    private bool isActive = true;
+
     protected override void ExecuteAction(CharacterBehaviour character)
     {
-        Debug.Log("Pedestal");
+        if(isActive)
+        {
+            isActive = false;
+            if(character && character.CheckInventaryObjectOnSelectedPosition("Robot"))
+            {
+                robotGameObject.SetActive(true);
+            }
+            else if (character && character.CheckInventaryObjectOnSelectedPosition("Machine"))
+            {
+                machineGameObject.SetActive(true);
+            }
+            else if (character && character.CheckInventaryObjectOnSelectedPosition("Lamp"))
+            {
+                lampGameObject.SetActive(true);
+            }
+            else if (character && character.CheckInventaryObjectOnSelectedPosition("Dog"))
+            {
+                dogGameObject.SetActive(true);
+            }
+            else
+            {
+                Debug.Log("None");
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isActive = true;
     }
 }
